c4a2ffa7a137adafd2caec3c104966a1c31ffd52
Jatioi <Jsuzibloguen@gmail.com>
2023-09-13 16:38:44 +0800

    About-Array_finding_Max_Value

---
+++ about_array/question_6.rb
@@ -0,0 +1,8 @@
+# Find Maximum Value:
+# Write a program that finds and displays the maximum value in an array.
+
+my_array = [1, 54, 2, 40 ,5, 200, 3]
+
+my_array.max
+
+puts "The maximum value of the array is #{my_array.max}"
