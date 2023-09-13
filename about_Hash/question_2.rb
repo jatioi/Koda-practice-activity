b5a07de2d5d423276f1171c91c0a86f4342b594a
Jatioi <Jsuzibloguen@gmail.com>
2023-09-13 16:57:14 +0800

    About-Hash_SettingValue-For-SpecificKey

---
+++ about_Hash/question_2.rb
@@ -0,0 +1,17 @@
+
+#[] = Method : Set a value for a specific key.
+# #Create an empty hash to represent a shopping list.Add items and quantities to the list using keys for items and values for quantities
+
+# shopping_list = {} #if it's a list you should put the {} in the initial state
+#   shopping_list ["egg"] = 12
+#   shopping_list ["apple"] = 3
+#   shopping_list ["tuna"] = 1
+#
+# puts shopping_list.inspect
+
+my_list = {}
+my_list ["pen"] = 3
+my_list ["notebook"] = 1
+
+puts my_list.inspect
+
