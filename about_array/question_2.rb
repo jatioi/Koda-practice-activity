af2aa8fbce9b03f12ff6002ab7ba155cf4744085
Jatioi <Jsuzibloguen@gmail.com>
2023-09-13 16:41:10 +0800

    About-Array_checking_value_if_exist

--- about_array/question_2.rb
+++ about_array/question_2.rb
@@ -1,5 +1,5 @@
-# Write a Ruby program to check whether a value exists in an array.
-
+# # Write a Ruby program to check whether a value exists in an array.
+# .include method
 numbers = [1, 3, 5, 6, 12, 122, 42, 30]
 
 value_to_check = 5
@@ -8,6 +8,16 @@ if numbers.include?(value_to_check)
   puts "#{value_to_check} Does exist in the array"
 else
   puts "#{value_to_check}does not exist in the array "
+end
+
+numbers = [122, 130, 2, 5, 2, 500, 10]
+
+Value_to_check = 3
+
+if numbers.include? (Value_to_check)
+  puts "#{Value_to_check} Meron sa Array "
+else
+  puts "#{Value_to_check} Wala Siya sa Array "
 end
 
 
