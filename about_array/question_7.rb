ddbd3881a6e9d24ee3a071c61e2e4aa549bb93e4
Jatioi <Jsuzibloguen@gmail.com>
2023-09-13 16:47:37 +0800

    About-Array_Reverse-Array

---
+++ about_array/question_7.rb
@@ -0,0 +1,9 @@
+# Reverse Array:
+# Create a program to reverse the elements of an array and display the reversed array.
+
+orig_array = [ 1, 2, 3, 4, 5]
+
+rev_array = orig_array.reverse
+
+puts "The original array is #{orig_array}"
+puts "The reversed array is #{rev_array}"
