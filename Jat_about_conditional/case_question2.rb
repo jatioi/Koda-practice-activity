2960756e3af6ac3f3e6d09fb792111e6c1dbc14e
Jatioi <Jsuzibloguen@gmail.com>
2023-09-15 14:15:28 +0800

    case_question2

---
+++ Jat_about_conditional/case_question2.rb
@@ -0,0 +1,2 @@
+#Write a program to determine the season based on the month entered by the user.
+# Use a case statement to print "Spring," "Summer," "Fall," or "Winter" depending on the month (e.g., March, June, September, December).
