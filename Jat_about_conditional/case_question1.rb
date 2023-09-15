dd51e6086646ea009153d79fd33c313615fa5fa7
Jatioi <Jsuzibloguen@gmail.com>
2023-09-15 14:15:08 +0800

    case_question1

---
+++ Jat_about_conditional/case_question1.rb
@@ -0,0 +1,7 @@
+#Create a program that asks the user to enter a day of the week (e.g., "Monday," "Tuesday").
+# Use a case statement to determine if it's a weekday or weekend. If it's a weekday (Monday to Friday),
+# print "It's a weekday." If it's a weekend (Saturday or Sunday), print "It's the weekend."
+
+
+week = ("monday" "tuesday" "wednesday" "thursday" "friday")
+
