ca1cb23b22a4694c243dc65e1514956203da46dd
Jatioi <Jsuzibloguen@gmail.com>
2023-09-15 14:16:07 +0800

    if,else_question2

---
+++ Jat_about_conditional/if,else_question1.rb
@@ -0,0 +1,5 @@
+
+
+#Create a program that checks if a user's age is greater than or equal to 21. If it is,
+# -print "You are eligible to enter the bar." Otherwise, print "You are not eligible to enter the bar."
+
