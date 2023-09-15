ce6d5fc05e7e3875c40f69450f49ce61cba44a8f
Jatioi <Jsuzibloguen@gmail.com>
2023-09-15 14:16:21 +0800

    if,else_question3

---
+++ Jat_about_conditional/if,else_question3.rb
@@ -0,0 +1,2 @@
+#Build a simple ticket pricing program for a movie theater. Ask the user for their age and whether it's a matinee (morning) show.
+# If the user is 60 or older or it's a matinee show, charge $5 for the ticket; otherwise, charge $10.
