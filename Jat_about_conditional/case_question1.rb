bfba5436386e25c2bbc854db75babcfbd9e06448
Jatioi <Jsuzibloguen@gmail.com>
2023-09-18 13:07:51 +0800

    determinining-week

--- Jat_about_conditional/case_question1.rb
+++ Jat_about_conditional/case_question1.rb
@@ -2,6 +2,13 @@
 # Use a case statement to determine if it's a weekday or weekend. If it's a weekday (Monday to Friday),
 # print "It's a weekday." If it's a weekend (Saturday or Sunday), print "It's the weekend."
 
+puts "Enter any day of the week"
+day = gets.chomp.downcase.capitalize
 
-week = ("monday" "tuesday" "wednesday" "thursday" "friday")
+case day
+when "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+  puts "It's a weekday"
+when "Saturday", "Sunday"
+  puts "It's the weekend"
+end
 
