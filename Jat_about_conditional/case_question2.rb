77ace0f8c3be70e053fea28b9bf64af3aaca677d
Jatioi <Jsuzibloguen@gmail.com>
2023-09-18 13:09:32 +0800

    case-determining-season

--- Jat_about_conditional/case_question2.rb
+++ Jat_about_conditional/case_question2.rb
@@ -1,2 +1,16 @@
 #Write a program to determine the season based on the month entered by the user.
 # Use a case statement to print "Spring," "Summer," "Fall," or "Winter" depending on the month (e.g., March, June, September, December).
+
+puts "Type any month and I will tell what's the season"
+month =gets.chomp.downcase.capitalize
+
+case month
+when "January", "February", "December"
+  puts "During the month of #{month} it is winter"
+when "April", "May", "March"
+  puts "During the month of #{month} it is spring"
+when "June", "July"
+  puts "During this time of the month it is Summer"
+when "September", "October", "November"
+  puts "During this time it is Autumn"
+end
