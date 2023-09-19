f6225fc26e82278b985fa5bf138d491a311dcab5
Jatioi <Jsuzibloguen@gmail.com>
2023-09-19 13:57:51 +0800

    Checks-if-word-contains-string'ruby'

--- Jat_about_conditional/unless_question3.rb
+++ Jat_about_conditional/unless_question3.rb
@@ -1,2 +1,12 @@
-#Build a program that checks if a given string contains the word "ruby." If it doesn't contain "ruby,"
+#Build a program that checks if a given string contains the word "ruby."
+# If it doesn't contain "ruby,"
 # print "No 'ruby' found." Otherwise, print "Found 'ruby' in the string."
+
+puts "please enter a string"
+user_input = gets.chomp
+
+if user_input.include? ("ruby")
+  puts "I found the string 'ruby' in the string"
+else
+  puts "no 'ruby' found"
+end
