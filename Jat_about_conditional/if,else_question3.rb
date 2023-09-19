931c54d283d02e559859f41f71cdd79ba6bd35f9
Jatioi <Jsuzibloguen@gmail.com>
2023-09-19 13:54:57 +0800

    simple-ticketing-program

--- Jat_about_conditional/if,else_question3.rb
+++ Jat_about_conditional/if,else_question3.rb
@@ -1,2 +1,17 @@
-#Build a simple ticket pricing program for a movie theater. Ask the user for their age and whether it's a matinee (morning) show.
-# If the user is 60 or older or it's a matinee show, charge $5 for the ticket; otherwise, charge $10.
+#Build a simple ticket pricing program for a movie theater.
+# Ask the user for their age and whether it's a matinee (morning) show.
+# If the user is 60 or older or it's a matinee show,
+# charge $5 for the ticket; otherwise, charge $10.
+
+puts "Hi there, how old are you?"
+age = gets.chomp.to_i
+
+puts "Are you gonna get a matinee show ticket? (yes/no)"
+matinee_show = gets.chomp
+
+ticket_price = if age >= 60 || matinee_show == "yes"
+                 5
+               else
+                 10
+               end
+puts "Okay then..your ticket cost $#{ticket_price}. Enjoy the movie!"
