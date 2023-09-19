cf089fa332e74ab744d4f3c07a1195debb11e960
Jatioi <Jsuzibloguen@gmail.com>
2023-09-19 13:56:52 +0800

    Checks-temperature-in-celsius

--- Jat_about_conditional/unless_question2.rb
+++ Jat_about_conditional/unless_question2.rb
@@ -1,2 +1,13 @@
-#Write a program that asks the user to enter a temperature in Celsius. Use an unless statement to check
-# if it's below freezing (0°C or lower). If it's freezing, print "It's freezing!" Otherwise, print "The temperature is okay."
+#Write a program that asks the user to enter a temperature in Celsius.
+# Use an unless statement to check
+# if it's below freezing (0°C or lower). If it's freezing,
+# print "It's freezing!" Otherwise, print "The temperature is okay."
+
+puts "I am going to tell if the temperature is freezing or not. Can you enter a temperature in Celsius?"
+temp_celsius = gets.chomp.to_f
+
+unless temp_celsius > 0
+  puts "It is freezing!"
+else
+  puts "The temperature is okay"
+end
