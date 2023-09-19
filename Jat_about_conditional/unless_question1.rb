78f514276e767010134699f9b33a1a769c9ab1f8
Jatioi <Jsuzibloguen@gmail.com>
2023-09-19 13:56:04 +0800

    Checks_user-age-for-website

--- Jat_about_conditional/unless_question1.rb
+++ Jat_about_conditional/unless_question1.rb
@@ -1,2 +1,12 @@
 #Create a program that checks if a user's age is less than 13. If it's less than 13,
 # print "You are too young for this website." Otherwise, print "Welcome to the website."
+
+puts "Hey welcome to this website! Can you input your age?:"
+age = gets.chomp.to_i
+
+unless age >= 13
+  puts "Sorry! you are too young for this website"
+else
+  puts "Welcome to this website"
+  puts "you can now proceed"
+end
