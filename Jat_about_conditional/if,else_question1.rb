51a75a2edcd15cad28284daa8682ce707905cf68
Jatioi <Jsuzibloguen@gmail.com>
2023-09-19 13:53:24 +0800

    checks-age-if-greater-21

--- Jat_about_conditional/if,else_question1.rb
+++ Jat_about_conditional/if,else_question1.rb
@@ -1,5 +1,13 @@
+#Create a program that checks if a user's age is greater than or equal to 21.
+# If it is, print "You are eligible to enter the bar." Otherwise,
+# print "You are not eligible to enter the bar."
 
 
-#Create a program that checks if a user's age is greater than or equal to 21. If it is,
-# -print "You are eligible to enter the bar." Otherwise, print "You are not eligible to enter the bar."
+puts "Please enter your age:"
+age = gets.chomp.to_i
 
+if age >= 21
+  puts "You are eligible to enter the bar."
+else
+  puts "You are not eligible to enter the bar."
+end
